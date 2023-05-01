272980266f257e435047b5023a024a18b57b6eee
Furkan <furkannamli@outlook.com>
2023-05-01 07:05:26 +0300

    star wars datalar çekildi tasarım eksik

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import Karakter from './components/Karakter';
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [karakter, setKarakter] = useState([]);
 
+  useEffect(() => {
+
+          axios
+          .get("https://swapi.dev/api/people/")
+          .then((res) => setKarakter(res.data))
+          .catch((err) => console.log("Data çekerken hata", err))
+
+  }, []);
+  
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      {karakter && karakter.length ? karakter.map((karakter) => (
+        <Karakter key={karakter.url} name={karakter.name} birth={karakter.birth_year} height={karakter.height} />
+      )) : <p>Veriler yükleniyor...</p>}
     </div>
   );
 }
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,33 @@
-// Karakter bileşeniniz buraya gelecek
+import React from "react";
+import styled from "styled-components";
+
+const KarakterContainer = styled.div`
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  border: 2px solid green;
+  width: 40%;
+  text-align:center;
+  display: flex;
+  flex-direction:column;
+  justify-content:center;
+  gap: 20px;
+  align_items: center;
+  flex-wrap: wrap;
+`;
+
+const KarakterAdi = styled.h2`
+  margin-top: 0;
+  display:flex;
+  justify-content: center;
+`;
+
+function Karakter(props){
+    return(
+        <KarakterContainer>
+            <KarakterAdi>{props.name} {props.birth}</KarakterAdi>
+        </KarakterContainer>
+    )
+}
+
+export default Karakter;
